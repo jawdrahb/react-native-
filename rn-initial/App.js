a7290a488461a84a85079d97a65988f70233b6e6
Ashish <ashishbhardwaj727@gmail.com>
2021-09-20 23:43:54 +0530

    - course goal app

--- rn-initial/App.js
+++ rn-initial/App.js
@@ -2,49 +2,64 @@ import React, { useState } from 'react';
 import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = React.useState('');
+  const [courseGoals, setCourseGoals] = React.useState([]);
+
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoal(enteredText);
+  }
+
+  const addGoalHandler = async () => {
+    await setCourseGoals(currentGoals => [...currentGoals, enteredGoal]);
+    await setEnteredGoal('');
+  }
+
   return (
-    <View style={styles.screen}>
-      <View 
-        style={{
-          backgroundColor: 'red',
-          flexGrow: 1,
-          justifyContent: 'center',
-          alignItems: 'center'
-        }}
-      >
-        <Text>1</Text>
-      </View>    
-      <View 
-        style={{
-          backgroundColor: 'blue',
-          flexGrow: 1,
-          justifyContent: 'center',
-          alignItems: 'center'
-        }}
-      >
-        <Text>2</Text>
-      </View>    
-      <View 
-        style={{
-          backgroundColor: 'green',
-          flexGrow: 1,
-          justifyContent: 'center',
-          alignItems: 'center'
-        }}
-      >
-        <Text>3</Text>
-      </View>    
+    <View style={styles.root}>
+      <View style={styles.containerRow}>
+        <TextInput
+          placeholder="Course Goal"
+          style={styles.input}
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
+        />
+        <Button title="ADD MORE" onPress={addGoalHandler} />
+      </View>
+      <View>
+        {
+          courseGoals && courseGoals.map((goal, index) =>
+            <View key={`goal-${index}`} style={styles.listItem}>
+              <Text >
+                {goal}
+              </Text>
+            </View>
+          )
+        }
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  screen: {
-    padding: 50, 
-    flexDirection: 'row', 
-    width: '80%', 
-    height: 300, 
-    justifyContent: 'space-around', 
-    alignItems: 'stretch'
+  root: {
+    padding: 60,
+  },
+  containerRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  input: {
+    borderRadius: 4,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    padding: 10,
+    width: '80%'
+  },
+  listItem: {
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: '#ccc',
+    borderColor: '#888',
+    borderWidth: 1,
   }
 });
