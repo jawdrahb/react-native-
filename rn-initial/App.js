492b941d1bec39f9b2b4f4d1e8a7ccfc14282aa6
Ashish <ashishbhardwaj727@gmail.com>
2021-09-20 23:11:58 +0530

    adding into column layouting

--- rn-initial/App.js
+++ rn-initial/App.js
@@ -1,21 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.screen}>
+      <View 
+        style={{
+          backgroundColor: 'red',
+          flexGrow: 1,
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        <Text>1</Text>
+      </View>    
+      <View 
+        style={{
+          backgroundColor: 'blue',
+          flexGrow: 1,
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        <Text>2</Text>
+      </View>    
+      <View 
+        style={{
+          backgroundColor: 'green',
+          flexGrow: 1,
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        <Text>3</Text>
+      </View>    
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  screen: {
+    padding: 50, 
+    flexDirection: 'row', 
+    width: '80%', 
+    height: 300, 
+    justifyContent: 'space-around', 
+    alignItems: 'stretch'
+  }
 });
